2560c4ba4c40854329510b5514ffe64d5f9b3f45
hyeonjun-L <beeminor60@gmail.com>
2024-08-16 23:05:32 +0900

    feat(#1): Satoshi font 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import localFont from 'next/font/local';
+import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const consola = localFont({
+  src: '../styles/font/Satoshi-Variable.ttf',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={consola.className}>{children}</body>
     </html>
   );
 }
