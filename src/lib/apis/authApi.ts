0023c7aa1ab82882857a1ce29ba9588f50ec2194
hyeonjun-L <beeminor60@gmail.com>
2024-08-25 10:43:58 +0900

    feat(#7): refresh token api 연결

--- src/lib/apis/authApi.ts
+++ src/lib/apis/authApi.ts
@@ -19,3 +19,23 @@ export const login = async (email: string) => {
     throw error;
   }
 };
+
+export const reissue = async () => {
+  const response = await fetch(`/api/auth/refresh`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    const error: FetchError = new Error(
+      errorData.message || '토큰 재발급 오류',
+    ) as FetchError;
+    error.status = response.status;
+    error.errorCode = errorData.errorCode || 'UNKNOWN_ERROR';
+    throw error;
+  }
+};
