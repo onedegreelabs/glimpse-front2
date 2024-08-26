1a31fc7fa677ce4bc906a5818588685cf383976d
hyeonjun-L <beeminor60@gmail.com>
2024-08-26 17:00:51 +0900

    feat(#10): scroll up btn 상단에서 사라지게 수정

--- src/components/ScrollUpBtn.tsx
+++ src/components/ScrollUpBtn.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { ArrowSVG } from '@/icons/index';
 import { usePathname } from 'next/navigation';
 
 function ScrollUpBtn() {
+  const [isVisible, setIsVisible] = useState(true);
   const pathname = usePathname();
   const currentPathname = pathname.split('/').at(-1);
 
@@ -14,16 +16,31 @@ function ScrollUpBtn() {
     });
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className="relative ml-auto mr-14 size-[1px]">
-      <button
-        type="button"
-        className={`${currentPathname === 'all' ? 'mb-4' : 'mb-[90px]'} fixed bottom-0 z-10 flex size-[46px] items-center justify-center rounded-full bg-yellow-primary`}
-        aria-label="scroll-up-button"
-        onClick={handleScrollUp}
-      >
-        <ArrowSVG className="size-[16.5px] -rotate-90 transform" />
-      </button>
+      {isVisible && (
+        <button
+          type="button"
+          className={`${currentPathname === 'all' ? 'mb-4' : 'mb-[90px]'} fixed bottom-0 z-10 flex size-[46px] items-center justify-center rounded-full bg-yellow-primary`}
+          aria-label="scroll-up-button"
+          onClick={handleScrollUp}
+        >
+          <ArrowSVG className="size-[16.5px] -rotate-90 transform" />
+        </button>
+      )}
     </div>
   );
 }
