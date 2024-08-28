f5f55161f871de517ec0348d4432124157448b1f
hyeonjun-L <beeminor60@gmail.com>
2024-08-28 23:44:49 +0900

    refactor(#15): intro expanded 기능 수정

--- src/app/[eventId]/_components/participants/Loading.tsx
+++ src/app/[eventId]/_components/participants/Loading.tsx
@@ -1,5 +1,5 @@
 function Loading() {
-  return <li className="h-40 w-full animate-pulse rounded-3xl bg-white/10" />;
+  return <li className="h-52 w-full animate-pulse rounded-3xl bg-white/10" />;
 }
 
 export default Loading;
