bd91deb23f36f81f56c8f2275858b0d480b45a0f
hyeonjun-L <beeminor60@gmail.com>
2024-09-07 03:03:29 +0900

    refactor(#34): svg 크기 및 마진 변경

--- src/app/signup/_components/URLMark.tsx
+++ src/app/signup/_components/URLMark.tsx
@@ -1,11 +1,11 @@
 import { LinkSVG } from '@/icons/index';
 
-function URLMark({ size }: { size?: string }) {
+function URLMark({ bgSize, svgSize }: { bgSize?: string; svgSize?: string }) {
   return (
     <div
-      className={`${size || 'size-6'} flex flex-shrink-0 items-center justify-center rounded-full bg-blue-B50`}
+      className={`${bgSize || 'size-6'} flex flex-shrink-0 items-center justify-center rounded-full bg-blue-B50`}
     >
-      <LinkSVG className="size-3 fill-yellow-primary" />
+      <LinkSVG className={`${svgSize || 'size-3'} fill-yellow-primary`} />
     </div>
   );
 }
