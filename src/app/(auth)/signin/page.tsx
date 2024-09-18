a04dc1612184c2745ff772e1641aa359fa995255
hyeonjun-L <beemnor60@gmail.com>
2024-09-19 03:49:35 +0900

    feat(#59): Router Back 버튼 구현

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,4 +1,3 @@
-import { ArrowSVG4 } from '@/icons/index';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import SigninFunnel from './_components/SigninFunnel';
@@ -13,11 +12,6 @@ const page = async () => {
 
   return (
     <main className="relative z-0 flex h-dvh w-full flex-col bg-white text-gray-B80">
-      <header className="px-4 pt-4">
-        <button type="button" aria-label="back-router">
-          <ArrowSVG4 className="size-6 fill-black" />
-        </button>
-      </header>
       <SigninFunnel eventId={eventId} />
       <div className="absolute top-0 -z-10 h-screen w-full bg-white" />
     </main>
